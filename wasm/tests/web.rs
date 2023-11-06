c5513ea46fed4cc1a12a7917a51ab96d1fc0fc34
decaplanet <contact@decaplanet.com>
2023-11-07 03:13:54 +0900

    test: add more unit test

--- wasm/tests/web.rs
+++ wasm/tests/web.rs
@@ -9,9 +9,10 @@ use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen_test]
-fn pass() {
-    let input = r#"## Heading2
+mod divide_top_headings {
+    #[wasm_bindgen_test]
+    fn random_line_breaks() {
+        let input = r#"## Heading2
 Hi everyone
 
 ### Heading 3
@@ -20,18 +21,54 @@ end of heading 3
 #### Heading 4
 ## Heading 2"#;
 
-    let expected_output = vec![
-        vec![
+        let expected_output = vec![
+            vec![
+                "## Heading2",
+                "Hi everyone",
+                "### Heading 3",
+                "end of heading 3",
+                "#### Heading 4",
+            ],
+            vec!["## Heading 2"],
+        ];
+
+        assert_eq!(divide_top_headings(input), expected_output);
+    }
+
+    #[wasm_bindgen_test]
+    fn no_subheadings() {
+        let input = r#"## Heading2
+Hi everyone
+## Heading 2"#;
+
+        let expected_output = vec![vec!["## Heading2", "Hi everyone"], vec!["## Heading 2"]];
+
+        assert_eq!(divide_top_headings(input), expected_output);
+    }
+
+    #[wasm_bindgen_test]
+    fn single_section() {
+        let input = r#"## Heading2
+Hi everyone
+### Subheading
+Text under subheading"#;
+
+        let expected_output = vec![vec![
             "## Heading2",
             "Hi everyone",
-            "",
-            "### Heading 3",
-            "",
-            "end of heading 3",
-            "#### Heading 4",
-        ],
-        vec!["## Heading 2"],
-    ];
+            "### Subheading",
+            "Text under subheading",
+        ]];
 
-    assert_eq!(divide_top_headings(input), expected_output);
+        assert_eq!(divide_top_headings(input), expected_output);
+    }
+
+    #[wasm_bindgen_test]
+    fn empty_input() {
+        let input = "";
+
+        let expected_output: Vec<Vec<&str>> = vec![];
+
+        assert_eq!(divide_top_headings(input), expected_output);
+    }
 }
