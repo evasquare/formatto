cb67cfd48c8e93f7788c56193daae2d2d5f77d87
decaplanet <contact@decaplanet.com>
2023-11-07 00:23:19 +0900

    feat: implement "divide_top_headings" function

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -13,11 +13,48 @@ pub fn status() -> bool {
     true
 }
 
+macro_rules! console_log {
+    ($($arg:tt)*) => (log(&format!($($arg)*)));
+}
+
+pub fn divide_top_headings(input: &str) -> Vec<Vec<&str>> {
+    let input_lines = input.trim().split('\n').collect::<Vec<&str>>();
+
+    console_log!("{:#?}", &input_lines);
+
+    //* Check top heading level.
+    let top_heading_level = input_lines[0].chars().take_while(|&c| c == '#').count();
+    let top_heading_sharp = "#".repeat(top_heading_level);
+
+    console_log!("Top heading level: {}", top_heading_sharp);
+
+    //* Dividing documents into sections
+    let mut sections = Vec::<Vec<&str>>::new();
+    let mut current_section = Vec::<&str>::new();
+
+    for line in input_lines {
+        let is_top_heading = line.starts_with("##") && !line.starts_with("###");
+
+        if is_top_heading && !current_section.is_empty() {
+            sections.push(current_section);
+            current_section = Vec::<&str>::new();
+        }
+
+        current_section.push(line);
+    }
+
+    if !current_section.is_empty() {
+        sections.push(current_section);
+    }
+
+    sections
+}
+
 #[wasm_bindgen]
 pub fn format_document(input: &str) -> String {
-    log(input);
+    let top_heading_sections = divide_top_headings(input);
 
-    // TODO: implement the formatting feature here.
-    let formatted_value = format!("# TEST\n{}", input);
-    formatted_value.to_string()
+    // TODO
+
+    input.to_string()
 }
--- wasm/tests/web.rs
+++ wasm/tests/web.rs
@@ -2,14 +2,36 @@
 
 #![cfg(target_arch = "wasm32")]
 
-use std::assert;
+use std::{assert, vec};
 
-use formatto_wasm::format_document;
+use formatto_wasm::divide_top_headings;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
-    assert!(format_document("test input") != "");
+    let input = r#"## Heading2
+Hi everyone
+
+### Heading 3
+
+end of heading 3
+#### Heading 4
+## Heading 2"#;
+
+    let expected_output = vec![
+        vec![
+            "## Heading2",
+            "Hi everyone",
+            "",
+            "### Heading 3",
+            "",
+            "end of heading 3",
+            "#### Heading 4",
+        ],
+        vec!["## Heading 2"],
+    ];
+
+    assert_eq!(divide_top_headings(input), expected_output);
 }
