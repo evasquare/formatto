c60111f4895d46c8a1b25b76cefb721111e00314
decaplanet <contact@decaplanet.com>
2023-11-17 13:55:05 +0900

    feat: change build feature config
    exclude "console_error_panic_hook" for deployment

--- wasm/src/utils.rs
+++ wasm/src/utils.rs
@@ -1,4 +1,5 @@
 #[allow(dead_code)]
+#[cfg(feature = "development")]
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
