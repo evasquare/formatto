7a0ec24c7a283ec2ca18526353bb548719708a9c
Samu <csonka.samuel@gmail.com>
2023-12-16 21:59:54 +0100

    complete comments

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,9 @@ import { Editor, Notice } from "obsidian";
 
 import { format_document } from "../wasm/pkg/formatto_wasm";
 import FormattoPlugin from "./main";
-import { FormattoPluginSettings } from "@settings/settingTypes";
 
 export class FormattoUtil {
-    private plugin: FormattoPlugin;
-    settings: FormattoPluginSettings;
+    private plugin: FormattoPlugin;;
 
     constructor(plugin: FormattoPlugin) {
         this.plugin = plugin;
@@ -31,14 +29,10 @@ export class FormattoUtil {
         editor.setValue(formattedDocument);
         editor.setSelection(cursorPosition, cursorPosition);
         
-        let differentNotify = this.plugin.settings.additionalSettings.notifyText;
+        const differentNotify = this.plugin.settings.additionalSettings.notifyText;
 
-        if (differentNotify) {
-            if (originalDocument == editor.getValue()) {
-                new Notice("Document is already formatted!");
-            } else {
-                new Notice("Document Formatted!");
-            }
+        if (differentNotify && originalDocument === editor.getValue()) {
+            new Notice("Document is already formatted!");
         } else {
             new Notice("Document Formatted!");
         }
