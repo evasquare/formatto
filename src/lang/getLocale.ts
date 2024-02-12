2177beb1f5ed3f42a76a17022e16b730fed82e61
decaplanet <deca@decaplanet.com>
2024-02-12 19:11:14 +0900

    Make fallback and add Hungarian file

--- src/lang/getLocale.ts
+++ src/lang/getLocale.ts
@@ -1,5 +1,6 @@
 import de from "./locale/de.json";
 import en from "./locale/en.json";
+import hu from "./locale/hu.json";
 import ko from "./locale/ko.json";
 
 const detectedLanguage = window.localStorage.getItem("language");
@@ -20,11 +21,19 @@ type LocaleCategory = ObjectValues<typeof LOCALE_CATEGORY>;
 const locales: { [key: string]: typeof en } = {
     de: de,
     en: en,
+    hu: hu,
     ko: ko,
 };
 
 /** @example getLocale(LOCALE_CATEGORY.COMMANDS, "Format Document") */
 export const getLocale = (category: LocaleCategory, key: string) => {
     const usingLocale = locales[detectedLanguage] ?? locales.en;
+    const message = usingLocale[category][key];
+
+    if (message === "") {
+        const usingLocale = locales.en;
+        return usingLocale[category][key];
+    }
+
     return usingLocale[category][key];
 };
